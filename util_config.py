b0b693781863644a0ad42504385302b11b840057
dcantoka <candoga01@gmail.com>
2025-02-14 09:34:36 +0300

    dict fixed

--- util_config.py
+++ util_config.py
@@ -7,7 +7,7 @@ def create_yaml_from_secrets():
     config_path = "config.yaml"
 
     if not os.path.exists(config_path):
-        secrets = st.secrets._dict
+        secrets = st.secrets.to_dict()
 
         with open(config_path, "w") as yaml_file:
             yaml.dump(secrets, yaml_file, default_flow_style=False)
